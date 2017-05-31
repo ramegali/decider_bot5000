66165cde5f480d62b1f97fd4fda0291886a6ca37
Raphael Megali <ramegali23@gmail.com>
2017-05-30 20:02:44 -0400

    Removed unnecessary files, and cleaned up functions to make things a lot less recursive.

---
+++ README.md
@@ -0,0 +1,14 @@
+# DECIDER BOT 5000
+
+## FEATURES:
+**Decider Bot** is designed to randomly pick a food for you to eat, then
+list all available options near you using the ```Yelp Fusion``` API.
+It will then be possible to randomly pick from those available options as well.
+
+
+## AGGRESSIVE EXPANSION:
+- **Create user accounts** 
+	*for example:* Like the place? Save it, and then you can randomly pick a place from your favorites.
+- Give user option for picking cuisine? Maybe the user is already in the mood for something, but just needs to find a random place nearby that serves it.
+- Possible ideas for different decisions to be made: movies, what to
+  cook (recipes) 
--- decider_bot5000.py
+++ decider_bot5000.py
@@ -1,133 +1,107 @@
-# Created by Raphael Megali copyright 2017
-# VERSION 1.1
+# Raphael Megali copyright 2017
+# Version 2.0.1
 
-
-# Known issue(s) with this code:
-# >> ****FIXED**** Dictionary within dictionary is not working properly. Second dictionary's info
-# 		is overriding information in all other dictionary entries so that
-#		EVERY location ends up with the same website/address/phone.
-# >> Check for invalid city/state
+# gonna have to hide the access token etc
 
 from sys import exit
-from bs4 import BeautifulSoup
-import random
 import requests
+import random
+
+app_id = '39FHtcp39Nb-Khyzt6S_fw'
+app_secret = 'Gon9cUcLnZnecCwBvWEEP2rCPy5p9VTQabwfW9857k9l0ncLKbieRjw296U3Cia7'
+access_token = 'jC7h77ZwjBZfsg90k0JkTs3AWcwU1i2P3jJPtZEnYGlCabGOiG5dJntMml4-bACEunICfkGdK-hYbgL_R8Qn1CdySDgRMIsviLMepTccLe_Kpspi5XLK6zu90pgQWXYx'  # token.json()['access_token']
+url = 'https://api.yelp.com/v3/businesses/search'
+headers = {'Authorization': 'bearer %s' % access_token}
+
+
+# Don't really need this section, at least not for now,
+# since we already have the access token
+
+# data = {'grant_type': 'client_credentials',
+#        '39FHtcp39Nb-Khyzt6S_fw': app_id,
+#        'Gon9cUcLnZnecCwBvWEEP2rCPy5p9VTQabwfW9857k9l0ncLKbieRjw296U3Cia7': app_secret}
+# token = requests.post('https://api.yelp.com/oauth2/token', data=data)
 
 def decider_bot():
-	print "sigh . . ."
-	print "This is Decider Bot..."
-	print "I guess it's time to make a decision."
-	ready_status = raw_input("Are you ready? ")
-	if 'y' in ready_status:
-		food_decision = what_food()
-	else:
-		print "Take your time... I'll be around if you need me...."
-		exit()
-	print "So... would you like me to search nearby places for", food_decision, "?"	
-	ans = raw_input()
-	if 'y' in ans:
-		here_we_go(food_decision)
-	else:
-		print "Ok, fine."
-		exit()
+    print "sigh . . ."
+    print "This is Decider Bot..."
+    print "I guess it's time to make a decision."
+    ready_status = raw_input("Are you ready? ")
+    if 'y' in ready_status:
+        food_decision = what_food()
+    else:
+        print "Take your time... I'll be around if you need me...."
+        exit()
+    print "So... would you like me to search nearby places for", food_decision, "?"
+    ans = raw_input()
+    if 'y' in ans:
+        filters = get_filters(food_decision)
+    else:
+        print "Ok, fine."
+        exit()
+    get_results(filters)
+    print "Thank God that's finally over..."
 
-	print "Thank God that's finally over."
 
 def what_food():
-	food_choices = ['pizza', 'chinese', 'japanese', 'burgers', 'subs', 'italian', 'mexican',
-					 'thai', 'greek', 'sushi', 'seafood']
-	decision_index = random.randint(0, len(food_choices) - 1)
-	food_decision = food_choices[decision_index]
-	print "Okay\n\nHere's what I have decided for you:", food_decision
-	return food_decision
-
-def here_we_go(food_choice):
-	print "Alright. I'm going to need the name of your town/city/whatever: "
-	city = raw_input()
-	state = raw_input("And your state, if you don't mind: ")
-	number_of_pages = raw_input("How many pages of results would you like? ")
-	number_of_pages = int(number_of_pages)
-
-	print "Alright, I'll let Food Seeker Bot take it from here. . .\n\n"
-	list_of_results = food_seeker_bot(city.title(), state.upper(), number_of_pages, food_choice)
-	print "Thank you, Food Seeker Bot... Would you like to view Food Seeker Bot's full results (includes address, phone number, and link to webpage)?"
-	ans = raw_input()
-	if 'y' in ans:
-		display_results(list_of_results)
-	print "Would you like me to decide which of the above options to take?"
-	ans = raw_input()
-	if 'y' in ans:
-		final_destination(list_of_results)
-	else:
-		print "Ok have fun making decisions on your own."
-		exit()
+    food_choices = ['pizza', 'chinese', 'japanese', 'burgers', 'subs', 'italian', 'mexican',
+                    'thai', 'greek', 'sushi', 'seafood']
+    decision_index = random.randint(0, len(food_choices) - 1)
+    food_decision = food_choices[decision_index]
+    print "Okay\n\nHere's what I have decided for you:", food_decision
+    return food_decision
 
 
-def food_seeker_bot(city, state, number_of_pages, food_choice):
-	list_of_results = {}
-	page = 1
+def get_filters(food_choice):
+    filters = {}
+    print "Alright. Well, I'm going to need some parameters before we begin..."
+    location = raw_input("Please give me the zip code of the city/town where we're searching: ")
+    price = raw_input("On a scale of 1 to 4, how pricy do you wanna get? ")
+    # OTHER POSSIBILITIES INCLUDE:
+    # sort_by <-- parameter to sort by best_match (default), rating, review_count, or distance
+    # limit <-- number of businesses to return (default 20, max 50)
+    # transactions <-- can the place do delivery, pickup, or restaurant_reservation?
 
-	print "HELLO THIS IS FOOD SEEKER BOT"
-	print "PLEASE WAIT WHILE I SEEK YOUR FOODS"
-	while page <= number_of_pages:
-		print "\nPAGE", page
-		if page is 1:
-			url = 'http://www.yellowpages.com/search?search_terms=' + food_choice + '&geo_location_terms=' + city + '%2C+' + state
-		else:
-			url = 'http://www.yellowpages.com/search?search_terms=' + food_choice + '&geo_location_terms=' + city +'%2C%20' + state + '&page=' + str(page)
-		source_code = requests.get(url)
-		plain_text = source_code.text
-		soup = BeautifulSoup(plain_text, "html.parser")
-		# in findAll the 'a' flag stand for anchor which indicates where links are in html
-		for link in soup.findAll('a', {'class':'business-name'}):
-			href = "http://www.yellowpages.com" + link.get('href')
-			title = link.string
-			#print title  # <--- could be useful? basically a visual progress indicator
-			if title is None:
-				# gets rid of some addresses that don't work
-				continue
+    filters['location'] = location
+    filters['price'] = price
+    filters['term'] = food_choice
+    return filters
 
-			local_soup = soup_helper(href)
-			website = get_url(local_soup)
-			phone_num = get_phone(local_soup)
-			address = get_address(local_soup)
 
-			info = {} # this makes sure that info's data doesn't override every element in list_of_results
-			info['address'] = address
-			info['phone-number'] = phone_num
-			info['website'] = website
-			list_of_results[title] = info
+def get_results(filters):
+    params = {'location': filters['location'],
+              'term': filters['term'],
+              'pricing_filter': filters['price'],
+              # 'sort_by': 'rating'
+              }
 
-		page += 1
-	print "\nOKAY DONE\n\n"
-	return list_of_results
+    # get the info
+    resp = requests.get(url=url, params=params, headers=headers)
+    results = resp.json()['businesses']  # gets just the businesses
+    print "Would you like to see the results? Or no?"
+    ans = raw_input()
+    if 'y' in ans:
+        display_results(results)
 
-def soup_helper(url): # returns a soup obj
-    source_code = requests.get(url)
-    plain_text = source_code.text
-    return BeautifulSoup(plain_text, "html.parser")
+    your_destiny(results)
 
-def get_url(soup):
-    for link in soup.findAll('a', {'class':'secondary-btn website-link'}):
-        return link.get('href')
 
-def get_phone(soup):
-    for phone in soup.findAll('li', {'class':"phone primary"}):
-        return phone.string
+def display_results(results):
+    i = 0
+    print "Okay whatever, here they are: \n\n"
+    for placesf in results:
+        print "%s \n\tRATING: %s \n\tPRICE: %s \n\tADDRESS: %s, %s %s %s \n\tPHONE: %s \n" % (
+            results[i]['name'], results[i]['rating'], results[i]['price'],
+            results[i]['location']['address1'], results[i]['location']['city'], results[i]['location']['state'],
+            results[i]['location']['zip_code'],
+            results[i]['display_phone'])
+        i += 1
 
-def get_address(soup):
-    for i in soup.findAll('p', {'class':"adr"}):  # street name and num
-        return i.string
 
-def display_results(list_of_results):
-	print "Here are the results: "
-	for place in list_of_results:
-		print place, "\n\tAddress: %s\n\tPhone: %s\n\tWebsite: %s\n" %(list_of_results[place]['address'],list_of_results[place]['phone-number'],list_of_results[place]['website'])
-	print "\n\n"
+def your_destiny(results):
+    destiny = random.randint(0, len(results) - 1)
+    print "Alright, here's what I picked for you I hope you're satisfied: %s\n" % (results[destiny]['name'])
 
-def final_destination(list_of_results):
-	final_destination = random.choice(list(list_of_results.items()))
-	print "My final decision is ", final_destination, "\n\n"
 
-decider_bot()
-	
+if __name__ == "__main__":
+    decider_bot()
